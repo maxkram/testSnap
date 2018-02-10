49dbef7e63ff47c418768b6d506f162d40f399a3
maxkram <max.kramer@live.com>
2018-02-09 19:52:33 -0500

    app->application

--- source/app.js
+++ source/app.js
@@ -1,14 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import Application from './components/Application';
 
-const listOfItems = (
-  <ul className="list-of-items">
-    <li className="item-1">Item 1</li>
-    <li className="item-2">Item 2</li>
-    <li className="item-3">Item 3</li>
-  </ul>
-);
 ReactDOM.render(
-  listOfItems,
+  <Application />,
   document.getElementById('react-application'),
 );
